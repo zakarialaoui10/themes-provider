21240a022d334c80e7073d6a35d5327362765e28
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-14 10:31:16 +0100

    fix svelte

--- src/providers/svelte/index.svelte
+++ src/providers/svelte/index.svelte
@@ -2,12 +2,12 @@
   export let setTheme;
 </script>
 <script>
-// import { useRoot } from './use-root.js';
+import { useRoot } from 'ziko/use/use-root.js';
 export let theme;
 export let namespace = '';
 const root = useRoot(theme, { namespace });
 setTheme = (newTheme) => {
-root.use(newTheme);
+  root.use(newTheme);
 };
 </script>
 <slot />
---
+++ src/providers/vue/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import { useRoot } from './use-root.js'
+// import { ref } from 'vue'
+
+const props = defineProps({
+  theme: { type: Object, required: true },
+  namespace: { type: String, default: '' }
+})
+
+const root = useRoot(props.theme, { namespace: props.namespace })
+
+const setTheme = (newTheme) => {
+  root.use(newTheme)
+}
+
+defineExpose({ setTheme })
+</script>
+
+<template>
+  <slot />
+</template>
