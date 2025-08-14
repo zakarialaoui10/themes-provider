9ae580eee6e597028c2e0bac7dc16537add417e5
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-08-14 08:50:06 +0100

    remove example

--- src/use-theme.js
+++ src/use-theme.js
@@ -1,17 +1,3 @@
-const CosmicBlue = {
-    background: '#1B2836',
-    currentLine: '#223348',
-    selection: '#39516D',
-    foreground: '#C0D4E5',
-    comment: '#728CAB',
-    cyan: '#75A6FF',
-    green: '#5ED99F',
-    orange: '#FFAD6F',
-    pink: '#FF90B6',
-    purple: '#A889C5',
-    red: '#FF6E6E',
-    yellow: '#FCD256'
-}
 class ThemesManager {
     constructor(Theme, namespace = 'Ziko'){
         this.currentTheme = Theme;
@@ -25,12 +11,7 @@ class ThemesManager {
        const root = globalThis?.document?.documentElement?.style
        for(let prop in this.currentTheme){
            root.setProperty(`--${this.namespace}-${prop}`, this.currentTheme[prop]); 
-        //    console.log(`--${this.namespace}-${prop}`)
        }
     }
 }
-
-
 const useTheme = (Theme, namespace) => new ThemesManager(Theme, namespace);
-
-// let A = useTheme(CosmicBlue)
